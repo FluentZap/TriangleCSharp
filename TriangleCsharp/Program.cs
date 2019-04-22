356315d53e3efee81b5e03e853f7c09608b1ce80
Heather Zurek and Nathan Aden <nathan@>
2019-04-22 11:12:26 -0700

    add all logic

---
+++ TriangleCsharp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TriangleCsharp
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TriangleTest testthing = new TriangleTest();
+
+            Console.WriteLine("Welcone to triangle tester! Enter the first side: ");
+            int sideOne = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second side length: ");
+            int sideTwo = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter third side length: ");
+
+            int sideThree = int.Parse(Console.ReadLine());
+
+            string triangle = testthing.testTriangle(sideOne, sideTwo, sideThree);
+            Console.WriteLine(triangle);
+
+        }
+    }
+}
---
+++ TriangleCsharp/TriangleTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace TriangleCsharp
+{
+    class TriangleTest
+    {
+        public string testTriangle(int sideOne, int sideTwo, int sideThree)
+        {
+            int longestSide = 0;
+            int otherSide = 0;
+            if (sideOne > sideTwo && sideOne > sideThree) 
+            {
+                longestSide = sideOne;
+                otherSide = sideTwo + sideThree;
+            }
+               
+            if (sideTwo > sideOne && sideTwo > sideThree)
+            {
+                longestSide = sideTwo;
+                otherSide = sideOne + sideThree;
+            }
+                
+            if (sideThree > sideOne && sideThree > sideTwo)
+            {
+                longestSide = sideThree;
+                otherSide = sideOne + sideTwo;
+            }
+
+            if (otherSide < longestSide)
+                return "not a triangle";
+                
+
+            if (sideOne == sideTwo && sideTwo == sideThree)
+            {
+                return "This is an Equilateral";
+            }
+
+            if (sideOne == sideTwo || sideOne == sideThree || sideThree == sideTwo)
+            {
+                return "This is an Isosceles";
+            }
+
+
+
+            return "trianglething";
+        }
+    }
+}
