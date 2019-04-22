25f281510051e74dc80456c172f2cf0192998724
Heather Zurek and Nathan Aden <nathan@>
2019-04-22 14:50:20 -0700

    add new range and final logic

--- TriangleCsharp/NumberGame.cs
+++ TriangleCsharp/NumberGame.cs
@@ -3,20 +3,39 @@ namespace TriangleCsharp
 {
     public class NumberGame
     {
+        private int Min = 1;
+        private int Max = 100;
+        public int lastGuess = 0;
+
         Random rnd = new Random();
         public NumberGame()
         {
-            int number = rnd.Next(1, 100);
-			
+			//int number = rnd.Next(1, 100);
+            Guess();
+        }
+
+        public void GuessNumber(bool isHigher)
+        {
+            if (isHigher == true)
+            {
+                if (lastGuess >= Min) Min = lastGuess;
+            }
+            else
+            {
+                if (lastGuess <= Max) Max = lastGuess;
+            }
+            Guess();
         }
 
 
 
-
-        public int Guess() 
+        private int Guess() 
         {
-
-            return 0;
+            int Range = Max - Min;
+            float random = rnd.Next(90, 110) * 0.01f;
+            int numberGuess = (int)Math.Floor((Range / 2d) * random) + Min;
+            lastGuess = numberGuess;
+            return numberGuess;
         }
     }
 }
--- TriangleCsharp/Program.cs
+++ TriangleCsharp/Program.cs
@@ -33,25 +33,48 @@ namespace TriangleCsharp
 
         static void TestNumberGame()
         {
+            NumberGame myGame = new NumberGame();
             Console.WriteLine("Welcome to the thunder dome number guesser");
             Console.WriteLine("If i guess your number you die");
+            Console.WriteLine("Think of a number between 1 and 100 and press any key.");
+            Console.ReadKey(true);
 
-            Console.Write("Is your number higher or lower then: ");
-            Console.WriteLine("1. Higher  2.Lower  3.It is the number");
-            ConsoleKeyInfo answer = Console.ReadKey(true);
-
-            switch (answer.Key)
+            bool finish = false;
+            int count = 0;
+            while (finish == false)
             {
-                case ConsoleKey.D1:
-                    Console.WriteLine("One");
-                    break;
-                case ConsoleKey.D2:
-                    Console.WriteLine("Two");
-                    break;
-                case ConsoleKey.D3:
-                    Console.WriteLine("Three");
-                    break;
-            }
+                Console.WriteLine("Is your number higher or lower then: " + myGame.lastGuess);
+                Console.WriteLine("1. Higher  2.Lower  3.It is the number");
+                ConsoleKeyInfo answer = new ConsoleKeyInfo();
+                count++;
+
+                while(answer.Key != ConsoleKey.D1 && answer.Key != ConsoleKey.D2 && answer.Key != ConsoleKey.D3) 
+                {
+                    answer = Console.ReadKey(true);                    
+                }
+                switch (answer.Key)
+                {
+                    case ConsoleKey.D1:
+                        myGame.GuessNumber(true);
+                        break;
+                    case ConsoleKey.D2:
+                        myGame.GuessNumber(false);
+                        break;
+                    case ConsoleKey.D3:
+                        if (count < 5) {
+							Console.WriteLine("Your Die!");                            
+                        } 
+                        else
+                        {
+                            Console.WriteLine("Your Live to fight another day!");                            
+                        }
+                        finish = true;
+                        break;
+                    default:
+
+                        break;
+                }
+			}
         }
 
 		static void Main(string[] args)
